0d883736dfb1b23c1426acf6694863630b344e84
alexhad6 <Alex@Alexs-MacBook-Air.local>
2017-04-30 12:59:12 -0400

    Color changing

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-var name = "Anonymous";
-var value = 50;
+let name = 'Anonymous';
+let value = 50;
+let color = '#000000';
 
 function signIn(){
-    name = document.getElementById("nameInput").value;
-    if (name != ""){
-        value = document.getElementById("rangeInput").value;
-        document.getElementById("signIn").style.display = "none";
-        console.log(name+" "+value);
-    }
-    else {
-        name = "Anonymous";
-        value = document.getElementById("rangeInput").value;
-        document.getElementById("signIn").style.display = "none";
-        console.log(name+" "+value);
-    }
+    name = ($('#nameInput').val() == '') ? 'Anonymous' : $('#nameInput').val();
+    value = $('#rangeInput').val();
+    $('#signIn').hide();
+    console.log(name + ' ' + value);
 }
+
+$(document).ready(function(){
+    $('#rangeInput').change(function() {
+        let cVal = $('#rangeInput').val();
+        
+        if (cVal < 20) { color = '#0000ff'; } //left
+        else if (cVal < 40) { color = '#0000aa'; }//left-center
+        else if (cVal < 60) { color = '#000000'; } //center
+        else if (cVal < 80) { color = '#aa0000'; } //right-center
+        else if (cVal <= 100) { color = '#ff0000'; } //right
+
+        $('#nameInput').css('color', color);
+    });
+});
